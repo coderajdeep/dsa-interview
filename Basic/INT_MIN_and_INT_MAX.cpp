40c4d9dcf2dd52233757878cb47190ad5d022710
Rajdeep Mallick <87999365+coderajdeep@users.noreply.github.com>
2021-09-06 01:20:50 +0530

    Find INT_MIN and INT_MAX
    https://ide.geeksforgeeks.org/jQSiwFGrS4

---
+++ Basic/INT_MIN_and_INT_MAX.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+  
+    int int_max = (~0u) / 2;
+	  int int_min = -((~0u) / 2) - 1;
+	
+	  cout<<int_min<<"\n"<<INT_MIN<<"\n"<<int_max<<"\n"<<INT_MAX;
+    return 0;
+}

7fb0937bfa9942161e9be086d85f6b7cb30bd018
Rajdeep Mallick <87999365+coderajdeep@users.noreply.github.com>
2021-09-06 11:52:38 +0530

    Reverse an array
    https://ide.geeksforgeeks.org/N57MS4AZbn

---
+++ Array/reverse.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void Swap(int &a, int &b) {
+    int t = a;
+    a = b;
+    b = t;
+}
+
+void Reverse(int arr[], int n) {
+    int l = 0, r = (n - 1);
+    while(l<r) {
+        Swap(arr[l++], arr[r--]);
+    }
+}
+
+
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--) {
+        int n;
+        cin>>n;
+        int arr[n];
+        int arr2[n];
+        
+        for(int i=0; i<n; ++i)
+            cin>>arr[i];
+        
+        vector<int>arr3(arr, arr+n);
+        copy(arr3.begin(), arr3.end(), arr2);
+        
+        cout<<"Before reverse : ";
+        for(int val:arr) {
+            cout<<val<<" ";
+        }
+        cout<<"\n";
+        Reverse(arr, n);
+        cout<<"After reverse : ";
+        for(int val:arr) {
+            cout<<val<<" ";
+        }
+        cout<<"\n\n";
+        
+        
+        cout<<"Before reverse : ";
+        for(int val:arr2) {
+            cout<<val<<" ";
+        }
+        cout<<"\n";
+        
+        reverse(arr2, arr2+n);
+        
+        cout<<"After reverse : ";
+        for(int val:arr2) {
+            cout<<val<<" ";
+        }
+        cout<<"\n\n";
+        
+        cout<<"Before reverse : ";
+        for(int val:arr3) {
+            cout<<val<<" ";
+        }
+        cout<<"\n";
+        
+        reverse(arr3.begin(), arr3.end());
+        
+        cout<<"After reverse : ";
+        for(int val:arr3) {
+            cout<<val<<" ";
+        }
+        cout<<"\n\n";
+    }
+    return 0;
+}
